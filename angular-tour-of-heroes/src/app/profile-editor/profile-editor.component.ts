7bdcacb3f83be4e29afc25c6b1422f5d63b1e8b2
a33523 <hinlin888@gmail.com>
2018-11-01 13:12:06 +0800

    警告提示

--- angular-tour-of-heroes/src/app/profile-editor/profile-editor.component.ts
+++ angular-tour-of-heroes/src/app/profile-editor/profile-editor.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
-export class ProfileEditorComponent implements OnInit {
+export class ProfileEditorComponent {
 
-  constructor() { }
 
-  ngOnInit() {
+
+  //追蹤一組FormControl的值與有效性狀態，假若組中的任一控件無效，那這整組就無效。
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    //向web控制台輸出一條警告訊息，其中每個對象會以字串的型式按照順序輸出到控制台。
+    console.warn(this.profileForm.value);
   }
 
 }
