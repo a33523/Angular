dfcb7b980fb3cc5e799970b1b461ee7d5bc7c38b
a33523 <hinlin888@gmail.com>
2018-11-01 14:04:54 +0800

    修正表單

--- angular-tour-of-heroes/src/app/profile-editor/profile-editor.component.ts
+++ angular-tour-of-heroes/src/app/profile-editor/profile-editor.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl,FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-editor',
   templateUrl: './profile-editor.component.html',
   styleUrls: ['./profile-editor.component.css']
 })
-export class ProfileEditorComponent implements OnInit{
-  constructor() { }
+export class ProfileEditorComponent implements OnInit {
+
+
+  //FormBuilder 是一個服務元件，
+  //讓使用者自行創件表單而非初始就建立，簡化建構程式的樣板量。
+  constructor(private ab: FormBuilder) { }
 
   ngOnInit() {
   }
@@ -32,6 +36,16 @@ export class ProfileEditorComponent implements OnInit{
     // TODO: Use EventEmitter with form value
     //向web控制台輸出一條警告訊息，其中每個對象會以字串的型式按照順序輸出到控制台。
     console.warn(this.profileForm.value);
+  }
+
+  updateProfile() {
+    //patchValue 可以用對像中所定義的任何屬性為表單模型進行替換修正。
+    this.profileForm.patchValue({
+      firstName: 'Nancy',
+      address: {
+        street: '123 Drew Street'
+      }
+    });
   }
 
 }
